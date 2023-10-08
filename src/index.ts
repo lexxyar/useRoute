003555109f63103c0fcd2ddba7b619c7853785d3
Aleksey Komov <lexxyar@ya.ru>
2023-10-08 16:06:23 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import Router from "./lib/Router";
 import RoutConfig from "./lib/RoutConfig";
 // import route from "./lib/functions";
 
-function route(name: string): string {
+export const route = function (name: string): string {
     Router.fetch()
     const rt: Route | undefined = Router.findByName(name)
     console.log('route', name, rt, Route)
     return rt ? rt.url : '#'
 }
 
-export {RoutConfig, Router, Route, route}
+export {RoutConfig, Router, Route}
+
